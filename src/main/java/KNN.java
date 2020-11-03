1238ca8fa142ea9c4b23e3aad561bf8e499b55d1
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-11-03 16:08:17 -0500

    added failing cartisan

--- src/main/java/KNN.java
+++ src/main/java/KNN.java
@@ -12,16 +12,20 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+// import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.util.MLUtils;
 
 import scala.Tuple2;
 
-public class WordCount {
+public class KNN {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length != 4) { // TODO add check that instances of train and test are same length for each data point
@@ -34,13 +38,48 @@ public class WordCount {
 		Integer k = Integer.valueOf(args[3]); // k for kNN
 
 		SparkConf conf = new SparkConf().setAppName("KNN Spark");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+		SparkContext sc = new SparkContext(conf);
 
-		final Broadcast<Integer> broadcastK = sc.broadcast(k);
+		// final Broadcast<Integer> broadcastK = sc.broadcast(k);
 		
 		// Load and parse the data files.
 		JavaRDD<LabeledPoint> trainData = MLUtils.loadLibSVMFile(sc, args[0]).toJavaRDD();
-		JavaRDD<LabeledPoint> testData = MLUtils.loadLibSVMFile(sc, args[2]).toJavaRDD();
+		JavaRDD<LabeledPoint> testData = MLUtils.loadLibSVMFile(sc, args[2]).toJavaRDD(); // TODO what type are the values? and keys?
+
+		trainData.cache();
+		// int trainingFrequency = trainData.groupBy("label").count();
+		// System.out.println("trainingFrequency: " + trainingFrequency);
+
+
+		// System.out.println(trainData.rdd);
+		System.out.println("HERE training data rdd: " + trainData.toString());
+
+		// collect RDD for printing
+        for(LabeledPoint line:trainData.collect()){
+            System.out.println("* "+line.toString);
+        } 
+
+		// trainData.show();
+
+		JavaPairRDD<LabeledPoint,LabeledPoint> cart = trainData.cartesian(testData);
+		cart.saveAsTextFile(args[1]+"/cart");
+		
+		JavaPairRDD<String,Tuple2<Double,String>> knnMapped =
+            //                              input                  K       V
+            cart.mapToPair(new PairFunction<Tuple2<String,String>, String, Tuple2<Double,String>>() {
+			@Override
+			public Tuple2<String,Tuple2<Double,String>> call(Tuple2<String,String> cartRecord) {
+				LabeledPoint trainPoint = cartRecord._1;
+				LabeledPoint testPoint = cartRecord._2;
+				
+				double distance = Util.calcDistance(trainInstance, testInstance);
+				String K = rRecordID; //  r.recordID
+				Tuple2<Double,String> V = new Tuple2<Double,String>(distance, trainPoint.getLabel()); // TODO not string?
+				return new Tuple2<String,Tuple2<Double,String>>(K, V);
+			}
+		});
+		knnMapped.saveAsTextFile(args[1]+"/knnMapped");
+
 
 
 		// JavaRDD<String> trainFile = sc.textFile(args[0]);
@@ -58,10 +97,10 @@ public class WordCount {
 		// 	public Integer call(Integer a, Integer b) { return a + b; }
 		// });
 		
-		counts.saveAsTextFile(args[1]);
+		// counts.saveAsTextFile(args[1]);
 
 		long endTime = System.nanoTime();
-			long totalTime = (endTime - startTime)/1000000;
-			computeAccuracy(args[1], totalTime);
+		long totalTime = (endTime - startTime)/1000000;
+		// computeAccuracy(args[1], totalTime);
 	}
 }
---
+++ src/main/java/Util.java
@@ -0,0 +1,18 @@
+package com.alwin;
+
+import org.apache.spark.mllib.regression.LabeledPoint;
+
+
+public class Util {
+    public static double calcDistance(LabeledPoint trainInstance, LabeledPoint testInstance) {
+        // calc distance
+        double distance = 0;
+        for (int i = 0; i < trainInstance.getFeatures().size() - 1; i++) {
+            double diff = Double.parseDouble(testInstance.getFeatures().apply(i)) - Double.parseDouble(trainInstance.getFeatures().apply(i));
+            distance += diff * diff;
+        }
+
+        distance = Math.sqrt(distance);
+        return distance;
+    }
+}
