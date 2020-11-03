8bd8c735d8612522295eeaaf85078e17ec2d889d
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-11-03 12:18:36 -0500

    removed dead code

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,6 +1,6 @@
 package com.alwin;
 
-import java.io.File;
+import java.io.File; // TODO remove unneeded imports
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -25,53 +25,23 @@ public class WordCount {
 
 	public static void main(String[] args) throws Exception
 	{
-		makeLIBSVMFiles();
-		// SparkConf conf = new SparkConf().setAppName("Word Count Spark");
-		// JavaSparkContext sc = new JavaSparkContext(conf);
+		SparkConf conf = new SparkConf().setAppName("Word Count Spark");
+		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		// JavaRDD<String> textFile = sc.textFile(args[0]);
+		JavaRDD<String> textFile = sc.textFile(args[0]);
 		
-		// JavaRDD<String> words = textFile.flatMap(new FlatMapFunction<String, String>() {
-		// 	public Iterator<String> call(String s) { return Arrays.stream(s.split(" ")).iterator(); }
-		// });
+		JavaRDD<String> words = textFile.flatMap(new FlatMapFunction<String, String>() {
+			public Iterator<String> call(String s) { return Arrays.stream(s.split(" ")).iterator(); }
+		});
 		
-		// JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
-		// 	public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s, 1); }
-		// });
+		JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+			public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s, 1); }
+		});
 		
-		// JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-		// 	public Integer call(Integer a, Integer b) { return a + b; }
-		// });
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			public Integer call(Integer a, Integer b) { return a + b; }
+		});
 		
-		// counts.saveAsTextFile(args[1]);
-	}
-
-	public static void makeLIBSVMFiles() throws FileNotFoundException, IOException {
-		String[] inputFileNames = {"superSmallTrain copy.txt", "smallTrain copy.txt", "mediumTrain copy.txt", "largeTrain copy.txt"};
-		String[] outputFileNames = {"superSmallTrain.txt", "smallTrain.txt", "mediumTrain.txt", "largeTrain.txt"};
-	
-		for (int i = 0; i < inputFileNames.length; i++) {
-			File inputFile = new File(inputFileNames[i]);
-			Scanner myFileReader = new Scanner(inputFile);
-
-			FileWriter outputFile = new FileWriter(outputFileNames[i]);
-			// BufferedWriter bw = new BufferedWriter(outputFile);
-		    // PrintWriter out = new PrintWriter(bw);
-			while (myFileReader.hasNextLine()) {
-				String currentLine = myFileReader.nextLine();
-				System.out.println(currentLine);
-				String[] values = currentLine.split(",");
-				StringBuilder result = new StringBuilder("");
-				result.append(values[values.length - 1]);
-				for (int j = 0; j < values.length- 1; j++) {
-					result.append(" " + (j+1) + ":" + values[j]);
-				}
-				result.append("\n");
-				// out.println(result.toString());
-				outputFile.write(result.toString());
-			}
-			myFileReader.close();
-			outputFile.close();
-		}
+		counts.saveAsTextFile(args[1]);
 	}
 }
